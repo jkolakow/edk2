40a7f4e749f5ef1f71717ecc40b236ddfa449bec
Jacek Kolakowski IIO <jacek.kolakowski@intel.com>
2025-10-03 17:32:07 +0200

    Revert "MdePkg: Add PCIe capability structures for DPC and DLF"

--- MdePkg/Include/IndustryStandard/Pci30.h
+++ MdePkg/Include/IndustryStandard/Pci30.h
@@ -42,7 +42,7 @@
 ///
 /// PCI Capability List IDs and records
 ///
-#define EFI_PCI_CAPABILITY_ID_PCIEXP  0x10  // Obsolete, use PCI_EXPRESS_CAPABILITY_ID
+#define EFI_PCI_CAPABILITY_ID_PCIEXP  0x10
 
 #pragma pack(1)
 
